9a104bceb70877893dccc6f7d7704bb3c532a2ee
teamalexander5 <agpollaci@icloud.com>
2025-10-07 22:00:08 -0500

    Finalized buttons. Created algae command groups.

--- src/main/java/frc/robot/commands/KelplerCommands.java
+++ src/main/java/frc/robot/commands/KelplerCommands.java
@@ -38,10 +38,18 @@ public class KelplerCommands {
         new InstantCommand(() -> Robot.coralSubsystem.setCoralAngle(Constants.CoralConstants.CORAL_SHOOT_ANGLE), Robot.coralSubsystem), 
         new InstantCommand(() -> Robot.elevator.setElevatorPose(Constants.ElevatorConstants.ELEVATOR_HIGH_GOAL), Robot.elevator)
     );
-
-    public static final Command algaeRemove = new SequentialCommandGroup(
+    
+    // check for increase in roller speed
+    public static final Command removeL2Algae = new SequentialCommandGroup(
         new InstantCommand(() -> Robot.coralSubsystem.setCoralAngle(Constants.AlgaeConstants.CLEAR_ALGAE_ANGLE), Robot.coralSubsystem),
-        new InstantCommand(() -> Robot.elevator.setElevatorPose(Constants.AlgaeConstants.ELEVATOR_CLEAR_ALGAE_LOW), Robot.elevator),
+        new InstantCommand(() -> Robot.elevator.setElevatorPose(Constants.AlgaeConstants.ELEVATOR_CLEAR_ALGAE_L2), Robot.elevator),
+        new RunCommand(() -> Robot.coralSubsystem.runRoller(0,0.3), Robot.coralSubsystem)
+    );
+
+    // check for increase in roller speed
+    public static final Command removeL3Algae = new SequentialCommandGroup(
+        new InstantCommand(() -> Robot.coralSubsystem.setCoralAngle(Constants.AlgaeConstants.CLEAR_ALGAE_ANGLE), Robot.coralSubsystem),
+        new InstantCommand(() -> Robot.elevator.setElevatorPose(Constants.AlgaeConstants.ELEVATOR_CLEAR_ALGAE_L3), Robot.elevator),
         new RunCommand(() -> Robot.coralSubsystem.runRoller(0,0.3), Robot.coralSubsystem)
     );
     //many other cool possibilities with "WaitUntilCommand"
